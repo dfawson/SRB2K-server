d0b052ac4e32b46ef366f3252841fb0ee4687545
Daniel Fawson <dannyfawson@gmail.com>
2022-02-25 12:20:06 -0700

    fixed self.dir() issue

--- srb2k_bot/cogs.py
+++ srb2k_bot/cogs.py
@@ -62,12 +62,12 @@ class music_player(commands.Cog, name='music player module'):
             shuffle_list = self.playlists[arg1]
             random.shuffle(shuffle_list)
             for item in shuffle_list:
-                self.queue.append('{0}/srb2k_bot/playlists/{1}/{2}.mp3'.format(self.dir(),arg1,item))
+                self.queue.append('{0}/srb2k_bot/playlists/{1}/{2}.mp3'.format(self.dir,arg1,item))
         elif arg2 not in self.playlists[arg1]:
             await ctx.send("I don't know that song.")
             return
         else:
-            self.queue.append('{0}/srb2k_bot/playlists/{1}/{2}.mp3'.format(self.dir(),arg1,item))
+            self.queue.append('{0}/srb2k_bot/playlists/{1}/{2}.mp3'.format(self.dir,arg1,item))
         
         voice_channel = ctx.author.voice.channel
         print(voice_channel)
--- srb2k_bot/sonic_bot.py
+++ srb2k_bot/sonic_bot.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import random
 
 class sonic_bot(commands.Bot):
     
